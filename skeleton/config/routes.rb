8b0e13442df7333c06db2f870f9f24484a72b7d7
sameehkhan <sameehkhan1@gmail.com>
2018-12-05 13:52:10 -0800

    Fix routes

--- skeleton/config/routes.rb
+++ skeleton/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # Your routes here!
   namespace :api, defaults: { format: :json } do
-    resources :gifts, only: [:index, :show]
+    resources :parties, only: [:index, :show]
     resources :guests, only: [:index, :show] do
-      resources :parties, only: :index
+      resources :gifts, only: :index
     end
-    resources :parties, only: [:show]
+    resources :gifts, only: [:show]
   end
 end
