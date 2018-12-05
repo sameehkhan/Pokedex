40a94e244b5c84ac87542d429c146f89c57219b0
sameehkhan <sameehkhan1@gmail.com>
2018-12-05 13:46:12 -0800

    Add routes

--- skeleton/config/routes.rb
+++ skeleton/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # Your routes here!
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    resources :gifts, only: [:index, :show]
+    resources :guests, only: [:index, :show] do
+      resources :parties, only: :index
+    end
+    resources :parties, only: [:show]
+  end
 end
