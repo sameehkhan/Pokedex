8e9c102ac3fd11772c8a2557e141e22400530c9b
sameehkhan <sameehkhan1@gmail.com>
2018-12-05 13:53:43 -0800

    Modify parties route

--- skeleton/config/routes.rb
+++ skeleton/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # Your routes here!
   namespace :api, defaults: { format: :json } do
-    resources :parties, only: [:index, :show]
     resources :guests, only: [:index, :show] do
       resources :gifts, only: :index
     end
     resources :gifts, only: [:show]
+    resources :parties, only: [:index, :show]
   end
 end
