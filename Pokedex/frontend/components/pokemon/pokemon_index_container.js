d69f6013fffe3193dada73f31409d01fc0d4345f
sameehkhan <sameehkhan1@gmail.com>
2018-12-06 11:44:09 -0800

    Pass props to pokemon index

--- Pokedex/frontend/components/pokemon/pokemon_index_container.js
+++ Pokedex/frontend/components/pokemon/pokemon_index_container.js
@@ -2,9 +2,9 @@ import { connect } from 'react-redux';
 import { selectAllPokemon } from '../../reducers/selectors';
 
 const mapStateToProps = state => ({
-
+  pokemon: selectAllPokemon(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  requestAllPokemon: () => dispatch(requestAllPokemon())
 });
