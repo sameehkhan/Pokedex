61ea9ac73889fc7f16f84644b7e4dd1e5d3b7736
sameehkhan <sameehkhan1@gmail.com>
2018-12-06 12:08:01 -0800

    Import pokemon index container into root

--- Pokedex/frontend/components/pokemon/pokemon_index.jsx
+++ Pokedex/frontend/components/pokemon/pokemon_index.jsx
@@ -1,5 +1,24 @@
-import { connect } from 'react-redux';
+import React from 'react';
 
-export const PokemonIndex = () => {
+class PokemonIndex extends React.Component {
+  componentDidMount() {
+    this.props.requestAllPokemon();
+  }
 
-};
+  render() {
+    // const chirps = this.props.chirps
+    const pokemon = this.props.pokemon; // so cool!
+
+    return (
+      <div className='pokemon-index'>
+        {
+          pokemon.map(poke => (
+            <li key={poke.id}>{poke.name}</li>
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+export default PokemonIndex;
--- Pokedex/frontend/components/root.jsx
+++ Pokedex/frontend/components/root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '../store/store';
+// import PokemonIndex from './pokemon/pokemon_index';
+import PokemonIndexContainer from './pokemon/pokemon_index_container';
 
 const store = configureStore();
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <div>Hello, world!</div>
+    <PokemonIndexContainer/>
   </Provider>
 );
 
