b80a57959b1fa0290f59e5c113ec93000e547e5f
sameehkhan <sameehkhan1@gmail.com>
2018-12-06 13:45:28 -0800

    Import hash router into root

--- Pokedex/frontend/components/root.jsx
+++ Pokedex/frontend/components/root.jsx
@@ -3,12 +3,16 @@ import { Provider } from 'react-redux';
 import { configureStore } from '../store/store';
 // import PokemonIndex from './pokemon/pokemon_index';
 import PokemonIndexContainer from './pokemon/pokemon_index_container';
+import { HashRouter, Route } from 'react-router-dom';
+
 
 const store = configureStore();
 
 const Root = ({ store }) => (
   <Provider store={store}>
+    <HashRouter></HashRouter>
     <PokemonIndexContainer />
+    </HashRouter>
   </Provider>
 );
 
