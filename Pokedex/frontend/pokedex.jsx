bc04b107286e36703875bac44f770aa2dad8c9cf
sameehkhan <sameehkhan1@gmail.com>
2018-12-06 13:43:53 -0800

    Import HashRouter

--- Pokedex/frontend/pokedex.jsx
+++ Pokedex/frontend/pokedex.jsx
@@ -4,6 +4,7 @@ import { configureStore } from './store/store';
 import { requestAllPokemon } from './actions/pokemon_actions';
 import { selectAllPokemon } from './reducers/selectors';
 import Root from './components/root';
+import { HashRouter, Route } from 'react-router-dom'; 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
